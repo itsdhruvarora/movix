e80968fefbc4db6efe80cba8466756721d2e2a9d
Dhruv Arora <101944782+itsdhruvarora@users.noreply.github.com>
2024-02-05 23:53:30 +0530

    setup redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+// import React from "react";
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 function App() {
-  return <div className="App">App</div>;
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log("DHRUV");
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+  return (
+    <div className="App">
+      App <br />
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
