1f77967ea3d8d2e130a7f81a656eecb1c789be3f
Dhruv Arora <101944782+itsdhruvarora@users.noreply.github.com>
2024-02-06 12:14:31 +0530

    done with hero section

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,5 +1,57 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./style.scss";
-export const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import Img from "../../../components/lazyLoadImage/Img";
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate;
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results[Math.floor(Math.random() * 20)].backdrop_path;
+    setBackground(bg);
+  }, [data]);
+  const searchQueryHandler = (event) => {
+    if (event.key == "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  return (
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore Now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show....."
+              onChange={() => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+
+            <button>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
 };
+
+export default HeroBanner;
